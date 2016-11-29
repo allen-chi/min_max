2f21baf2396249f2d008748d76108f9101542bcb
Ems <Ems>
2016-11-29 15:08:32 +0100

    updated

--- app/min_max.js
+++ app/min_max.js
@@ -1,1 +1,21 @@
+module.exports ={
+	findMinMax:function(arr){
+				var min = arr[0];
+		var max = arr[0];
+		var answer = [];
 
+		for(var i = 0; i<arr.length; i++){
+			if (arr[i] > max)
+				max = arr[i];
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		if (max==min){
+		  answer=[min];
+		return answer;
+	  }
+		answer=[min, max];
+		return answer;
+	}
+
+}
